e9442a07c53883e86449c9750c98b8f1dc659859
Mukul <varun.gupta8851401313@gmail.com>
2024-04-26 02:43:21 +0530

    Made Post component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home.jsx'
 import Navbar from './components/Navbar/Navbar.jsx';
 import Contact from './components/Contact/Contact.jsx';
-import Details from "./components/Details/Details.jsx"
+import Details from "./components/Details/Details.jsx";
+import Footer from "./components/Footer/Footer.jsx"
+import Posts from './components/Posts/Posts.jsx';
 
 function App() {
 
@@ -17,12 +19,13 @@ function App() {
        <Route path='/contact' exact element={<Contact/>} />
        <Route path='/home' exact element={<Home />} />
        <Route path='/details' exact element={<Details />} />
-       
+       <Route path='/posts' exact element={<Posts />} />
        {/* <Route path='/games' element={<Games/>}/>
        <Route path='/about' element={<About/>}/>
        <Route path='/sign-up' element={<SignUp/>}/> */}
      </Routes>
-     {/* <Footer /> */}
+     
+     <Footer />
    </Router> 
   )
 }
---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,26 @@
+
+
+
+const Posts = () => {
+    return (
+        <>
+            <div className="flex items-center justify-center">
+                <div className=" bg-[#005D9B] h-48 w-4/6 grid grid-cols-12 m-8 rounded-lg ">
+                    
+                    <div className=" col-span-6 ">
+                        <h1 className=" text-4xl text-white mt-9 px-9">Want to list more items?</h1>
+                        <p className="text-white px-9 py-5"> As a seller, list as many items as possible the increase the rate of getting more orders in order to be successful</p>
+                    </div>
+                    <div className=" col-span-6">
+                        <p className=" text-white mt-12">Write the name of the item and click continue</p>
+                        <span><input type="text" className=" rounded-sm w-60 text-2xl mt-4"/></span>
+                        <span><button className=" bg-[#0AD4EB] p-1 rounded-sm ml-3  hover:text-blue-200 text-lg">Continue</button></span>
+                    </div>
+                    </div>
+                </div>
+            
+        </>
+    )
+}
+
+export default Posts;
