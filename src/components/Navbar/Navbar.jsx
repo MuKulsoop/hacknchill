c8213f23be48ea0cae0a5c758d51cfc7944b6e53
KILLERTIAN <omsharma050322@gmail.com>
2024-05-13 22:45:30 +0530

    responsive nav changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
-import  { useState } from "react";
-import "./Navbar.css";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import './Navbar.css';
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [showModal, setShowModal] = useState(false);
 
-
-  const [isOpen, setIsOpen] = useState(false);
   const handleLoginButtonClick = () => {
     setShowModal(true);
   };
@@ -16,82 +16,120 @@ const Navbar = () => {
     console.log("Selected option:", option);
     setShowModal(false);
   };
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
   };
+
   return (
-    <nav>
-      <ul className="items">
-        <Link to="/"><div className="logo"><img src="https://res.cloudinary.com/djoebsejh/image/upload/v1715609494/Prescription_Testres/gjrmf6kwvf74khab1fgr.png" alt="" /></div></Link>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link
-            to="/posts"
-            className="flex flex-row justify-evenly items-center rounded-sm"
-          >
-            Products<ion-icon name="chevron-down-outline"></ion-icon>
-          </Link>
-          <ul>
-            <li>
-              <Link to="#" className="link">
-                Product{" "}
-              </Link>
-            </li>
-            <li>
-              <Link to="#" className="link">
-                Product{" "}
-              </Link>
-            </li>
-            <li>
-              <Link to="#" className="link">
-                Product{" "}
-              </Link>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <Link to="/contact">Consult Us</Link>
-        </li>
-        <li>
-          <Link to="#">Pricing </Link>
-        </li>
-        <li>
-          <Link to="/details" className="flex flex-row justify-evenly items-center">Resources <ion-icon name="chevron-down-outline"></ion-icon></Link>
-          <ul className="relative">
-            <li><Link to="#" className="link">Resources </Link></li>
-            <li><Link to="#" className="link">Resources </Link></li>
-            <li><Link to="#" className="link">Resources </Link></li>
-          </ul>
-        </li>
-      </ul>
-      <div className="flex flex-row items-center justify-centre">
-      <div className="relative">
-      <div className="customer-dropdown flex items-center text-gray-800 hover:text-gray-600">
-        <Link to="/contact" className="customer flex items-center">
-          <ion-icon name="earth-outline" className="mr-1"></ion-icon>
-          Customer
-          <ion-icon name="chevron-down-outline" className="ml-1"></ion-icon>
+    <nav className="bg-gray-100 text-black flex justify-between items-center p-4 relative">
+      <div className="logo flex flex-row">
+        <Link to='/'>
+          <img src="https://res.cloudinary.com/djoebsejh/image/upload/v1715609494/Prescription_Testres/gjrmf6kwvf74khab1fgr.png" alt="" className="w-12 h-12" />
         </Link>
-        {/* Dropdown Content */}
-        <ul className="absolute hidden bg-gray-200 shadow-md py-2 px-2 rounded-md mt-5 w-35 text-gray-800 customer-dropdown-menu top-5 flex items-center">
-          <li><Link to="#" className="block px-4 py-2 hover:bg-gray-100 rounded-md">Seller</Link></li>
+
+        <ul className={"mx-5 flex items-center " + ((isMobileMenuOpen || windowWidth <= 1025) ? 'hidden' : 'block')}>
+          <li className="relative mx-2">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="relative mx-2">
+            <Link to="/posts" className="flex items-center">
+              Products<ion-icon name="chevron-down-outline" className="ml-1"></ion-icon>
+            </Link>
+            <ul className="absolute hidden bg-gray-200 shadow-md py-2 px-2 rounded-md mt-5 w-35 text-gray-800">
+              <li><Link to="#" className="block px-4 py-2 hover:bg-gray-100 rounded-md">Product</Link></li>
+              <li><Link to="#" className="block px-4 py-2 hover:bg-gray-100 rounded-md">Product</Link></li>
+              <li><Link to="#" className="block px-4 py-2 hover:bg-gray-100 rounded-md">Product</Link></li>
+            </ul>
+          </li>
+          <li className="relative mx-2">
+            <Link to="/contact">Consult Us</Link>
+          </li>
+          <li className="relative mx-2">
+            <Link to="#">Pricing</Link>
+          </li>
+          <li className="relative mx-2">
+            <Link to="/details" className="flex items-center">Resources<ion-icon name="chevron-down-outline" className="ml-2"></ion-icon></Link>
+            <ul className="absolute hidden bg-gray-200 shadow-md py-2 px-2 rounded-md mt-5 w-35 text-gray-800">
+              <li><Link to="#" className="block px-4 py-2 hover:bg-gray-100 rounded-md">Resources</Link></li>
+              <li><Link to="#" className="block px-4 py-2 hover:bg-gray-100 rounded-md">Resources</Link></li>
+              <li><Link to="#" className="block px-4 py-2 hover:bg-gray-100 rounded-md">Resources</Link></li>
+            </ul>
+          </li>
         </ul>
+
       </div>
-    </div>
-        <div>{/* Modal */}</div>
+
+      {/* Mobile Menu Button */}
+      <button className="menuButton lg:hidden flex items-center justify-center" onClick={toggleMobileMenu}>
+        <ion-icon name={isMobileMenuOpen ? "close" : "menu-outline"} className="text-white text-2xl"></ion-icon>
+      </button>
+
+      {/* Mobile Menu */}
+      {isMobileMenuOpen && (
+        <div className="mobileMenu bg-gray-100 h-[90vh] p-10 sm:w-[500px] lg:w-full  absolute top-[10vh] right-0 flex flex-col items-center justify-center text-2xl transition-all duration-400 z-10">
+          <ul className="text-center">
+            <li className="my-4">
+              <Link to="/" onClick={toggleMobileMenu}>Home</Link>
+            </li>
+            <li className="my-4">
+              <Link to="/posts" onClick={toggleMobileMenu}>Products</Link>
+            </li>
+            <li className="my-4">
+              <Link to="/contact" onClick={toggleMobileMenu}>Consult Us</Link>
+            </li>
+            <li className="my-4">
+              <Link to="#" onClick={toggleMobileMenu}>Pricing</Link>
+            </li>
+            <li className="my-4">
+              <Link to="/details" onClick={toggleMobileMenu}>Resources</Link>
+            </li>
+            <div className="customer-dropdown flex items-center text-gray-800 hover:text-gray-600 my-4" onClick={toggleMobileMenu}>
+            <Link to="/contact" className="customer flex items-center">
+              <ion-icon name="earth-outline" className="mr-1"></ion-icon>
+              Customer
+              <ion-icon name="chevron-down-outline" className="ml-1"></ion-icon>
+            </Link>
+            <ul className="absolute hidden bg-gray-200 shadow-md py-2 px-2 rounded-md mt-5 w-35 text-gray-800 customer-dropdown-menu top-5 flex items-center">
+              <li><Link to="#" className="block px-4 py-2 hover:bg-gray-100 rounded-md">Seller</Link></li>
+            </ul>
+          </div>
+          <Link to="/login" className="logIn my-4" onClick={handleLoginButtonClick & toggleMobileMenu}>Log In</Link>
+          <Link to="/signup" className="signUp my-4" onClick={toggleMobileMenu}>Sign Up</Link>
+          </ul>
+        </div>
+      )}
+
+      <div className="hidden lg:flex items-center">
+        <div className="relative">
+          <div className="customer-dropdown flex items-center text-gray-800 hover:text-gray-600">
+            <Link to="/contact" className="customer flex items-center">
+              <ion-icon name="earth-outline" className="mr-1"></ion-icon>
+              Customer
+              <ion-icon name="chevron-down-outline" className="ml-1"></ion-icon>
+            </Link>
+            <ul className="absolute hidden bg-gray-200 shadow-md py-2 px-2 rounded-md mt-5 w-35 text-gray-800 customer-dropdown-menu top-5 flex items-center">
+              <li><Link to="#" className="block px-4 py-2 hover:bg-gray-100 rounded-md">Seller</Link></li>
+            </ul>
+          </div>
+        </div>
         <div className="customer-dropdown">
-        <Link to="/login" className="logIn" onClick={handleLoginButtonClick}>
-          Log In
-        </Link>
-
-
-        <Link to="/signup" className="signUp">
-          Sign Up
-        </Link>
-      </div>
-          {showModal && (
+          <Link to="/login" className="logIn" onClick={handleLoginButtonClick}>Log In</Link>
+          <Link to="/signup" className="signUp">Sign Up</Link>
+        </div>
+        {showModal && (
             <div className="modal fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50 ">
               <div className="modal-content bg-white p-8 rounded shadow-md">
                 <h2 className="text-2xl font-semibold mb-4">
@@ -133,7 +171,7 @@ const Navbar = () => {
               </div>
             </div>
           )}
-        </div>
+      </div>
     </nav>
   );
 };
